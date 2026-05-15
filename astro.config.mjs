a0d701ab575da9b0d8dd01515c91983765c10efe
theclstrickland <connor@redemptivedigital.com>
2026-05-15 14:58:21 -0400

    add plausible analytics and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://concordtreeremovalpros.com',
+  integrations: [sitemap()],
   build: {
     format: 'directory',
   },
